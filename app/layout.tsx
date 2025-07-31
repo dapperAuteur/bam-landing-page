189021a8c8532dc6f83789c2bda1a5d2276d264f
awe <aweful@awews.com>
2025-07-31 01:41:42 -0400

    add consolto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navigation from '../components/ui/Navigation'
 import ShareButton from '../components/share/ShareButton'
+import ConsoltoChat from "./../components/ConsoltoChat";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navigation />
         {children}
+        <ConsoltoChat />
         <ShareButton />
       </body>
     </html>
